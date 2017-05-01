1fcc7534eee89ba7f4dce485951b26377612e640
Mark Schuetze <markjschuetze@gmail.com>
2017-05-01 15:33:00 -0600

    an attempt to make this work

--- src/core/surgical-report.js
+++ src/core/surgical-report.js
@@ -1,17 +1,17 @@
 'use strict'
 
 var fs = require('fs')
-var Handlebars = require('handlebars/rutnime')
+var Handlebars = require('handlebars/runtime')
 
 module.exports.build = (data, callback) => {
   fs.readFile(function (err, content) {
     if (err) return callback(err)
     Handlebars.registerPartial('documents/surgical/page', Handlebars.template['documents/surgical/page'])
 
-    //Handlebars.registerPartial('layouts/base-html', Handlebars.template['layouts/base-html'])
-    //Handlebars.registerPartial('surgical/gross-description', Handlebars.template['surgical/gross-description'])
-    //Handlebars.registerPartial('surgical/surgical-diagnosis', Handlebars.template['surgical/surgical-diagnosis'])
-    //Handlebars.registerPartial('surgical/surgical-header', Handlebars.template['surgical/surgical-header'])
+    // Handlebars.registerPartial('layouts/base-html', Handlebars.template['layouts/base-html'])
+    // Handlebars.registerPartial('surgical/gross-description', Handlebars.template['surgical/gross-description'])
+    // Handlebars.registerPartial('surgical/surgical-diagnosis', Handlebars.template['surgical/surgical-diagnosis'])
+    // Handlebars.registerPartial('surgical/surgical-header', Handlebars.template['surgical/surgical-header'])
     var template = Handlebars.templates['documents/surgical/page']
 
     var result = template(data)
--- test/report-test.js
+++ test/report-test.js
@@ -6,7 +6,7 @@ const requisitions = require('../src/index.js')
 
 var inputData = require(path.join(__dirname, 'input-data'))
 
-describe('gynCytologyReq', function () {
+describe('surgicalReport', function () {
   before(function (done) {
     inputData.build(function (err, data) {
       if (err) return err
@@ -15,54 +15,12 @@ describe('gynCytologyReq', function () {
     })
   })
 
-  describe('gynCytologyReq', function () {
-    it('testing gynCytologyReq', function (done) {
-      requisitions.gynCytologyReq.build(inputData, function (err, result) {
+  describe('surgicalReport', function () {
+    it('testing surgicalReport', function (done) {
+      requisitions.surgicalReport.build(inputData, function (err, result) {
         if (err) console.log(err)
         assert.notEqual(result, null)
-        var requisitionPath = './requisitions/gyn-cytology-req.html'
-        fs.writeFile(requisitionPath, result, function (err) {
-          if (err) console.log(err)
-          done()
-        })
-      })
-    })
-  })
-
-  describe('surgicalReq', function () {
-    it('testing surgicalReq', function (done) {
-      requisitions.surgicalReq.build(inputData, function (err, result) {
-        if (err) console.log(err)
-        assert.notEqual(result, null)
-        var requisitionPath = './requisitions/surgical-req.html'
-        fs.writeFile(requisitionPath, result, function (err) {
-          if (err) console.log(err)
-          done()
-        })
-      })
-    })
-  })
-
-  describe('oncologyReq', function () {
-    it('testing oncologyReq', function (done) {
-      requisitions.oncologyReq.build(inputData, function (err, result) {
-        if (err) console.log(err)
-        assert.notEqual(result, null)
-        var requisitionPath = './requisitions/oncology-req.html'
-        fs.writeFile(requisitionPath, result, function (err) {
-          if (err) console.log(err)
-          done()
-        })
-      })
-    })
-  })
-
-  describe('ihcReq', function () {
-    it('testing ihcReq', function (done) {
-      requisitions.ihcReq.build(inputData, function (err, result) {
-        if (err) console.log(err)
-        assert.notEqual(result, null)
-        var requisitionPath = './requisitions/ihc-req.html'
+        var requisitionPath = './requisitions/surgical-report.html'
         fs.writeFile(requisitionPath, result, function (err) {
           if (err) console.log(err)
           done()
