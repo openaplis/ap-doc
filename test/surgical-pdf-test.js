7856e23247f5d9f4f264909ab4b8eb30700d1d0e
Mark Schuetze <mark.schuetze@YPIILab.YPII.com>
2017-06-01 15:03:33 -0600

    work on pdfkit

---
+++ test/surgical-pdf-test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert
+const path = require('path')
+const fs = require('fs')
+
+const surgicalReport = require('../src/core/pdf/base-pdf.js')
+
+
+describe('Surgical Report', function () {
+
+  it('Input data tests', function (done) {
+    //surgicalReport.new()
+    //assert.equal(surgicalReport, null)
+    done()
+  })
+
+})
